e749038c53cf1c6e43a70ed8c1926caa79f108df
Juan Jose Carranza Garcia <juancarranza7561@gmail.com>
2024-05-27 18:29:24 -0500

    Format script

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
-# bot.py
+#!/usr/bin/env python3
+""" Example bot.py """
 import os
-
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')  # Get the token from the .env file
 
 client = discord.Client(intents=discord.Intents.all())
 
+
 @client.event
 async def on_ready():
+    """ When the bot is ready """
     print(f'{client.user} has connected to Discord!')
     connected_guilds = len(client.guilds)
     print(f"{client.user} is connected to {connected_guilds} guilds.")
