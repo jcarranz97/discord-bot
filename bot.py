acd619d1fc9d2c9c01c01f075190e9d8800d031b
Juan Jose Carranza Garcia <juancarranza7561@gmail.com>
2024-05-27 18:36:04 -0500

    Bot as a custom class

--- bot.py
+++ bot.py
@@ -7,18 +7,19 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')  # Get the token from the .env file
 
-client = discord.Client(intents=discord.Intents.all())
+
+class MyBotClient(discord.Client):
+    """ Custom bot client """
+    async def on_ready(self):
+        """ When the bot is ready """
+        print(f'{self.user} has connected to Discord!')
+        connected_guilds = len(self.guilds)
+        print(f"{self.user} is connected to {connected_guilds} guilds.")
+        for guild in self.guilds:
+            print(f"  - {guild.name} (ID: {guild.id})")
+            for member in guild.members:
+                print(f"    - {member.name} (ID: {member.id})")
 
 
-@client.event
-async def on_ready():
-    """ When the bot is ready """
-    print(f'{client.user} has connected to Discord!')
-    connected_guilds = len(client.guilds)
-    print(f"{client.user} is connected to {connected_guilds} guilds.")
-    for guild in client.guilds:
-        print(f"  - {guild.name} (ID: {guild.id})")
-        for member in guild.members:
-            print(f"    - {member.name} (ID: {member.id})")
-
+client = MyBotClient(intents=discord.Intents.all())
 client.run(TOKEN)
