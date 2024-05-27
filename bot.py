22781e5a1aab711a1a61b655d8ffe0dbaecfe706
Juan Jose Carranza Garcia <juancarranza7561@gmail.com>
2024-05-27 18:26:16 -0500

    Include printing of servers (guilds) connected

--- bot.py
+++ bot.py
@@ -12,5 +12,9 @@ client = discord.Client(intents=discord.Intents.all())
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    connected_guilds = len(client.guilds)
+    print(f"{client.user} is connected to {connected_guilds} guilds.")
+    for guild in client.guilds:
+        print(f"  - {guild.name} (ID: {guild.id})")
 
 client.run(TOKEN)
